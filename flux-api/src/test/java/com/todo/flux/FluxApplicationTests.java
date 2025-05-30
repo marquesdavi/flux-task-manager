fcee7a8383e127e071f7f56e9532bc87de0ee879
marquesdavi <davibaterabatera@hotmail.com>
2025-05-30 20:07:34 -0300

    feature(api): Base api structure

---
+++ flux-api/src/test/java/com/todo/flux/FluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.flux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
